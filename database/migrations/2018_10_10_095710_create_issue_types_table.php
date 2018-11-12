20b5ef302c00b032064c16daa40a3722d3240c16
PetrVakulenko <vakulenkopetya@gmail.com>
2018-11-12 22:26:02 +0200

    [Diploma] Added issue types migrations table

---
+++ database/migrations/2018_10_10_095710_create_issue_types_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIssueTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('issue_types');
+    }
+}
